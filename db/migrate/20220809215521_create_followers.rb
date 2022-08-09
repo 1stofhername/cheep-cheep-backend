8fe0083a078c0a7e8ce9f34c5ae9877b80fe0184
Alicia <arrowsrp@yahoo.com>
2022-08-09 15:14:04 -0700

    Adds timestamps to followers table

--- db/migrate/20220809215521_create_followers.rb
+++ db/migrate/20220809215521_create_followers.rb
@@ -3,6 +3,7 @@ class CreateFollowers < ActiveRecord::Migration[6.1]
     create_table :followers do |t|
       t.integer :user_id
       t.integer :follower_id 
+      t.timestamps
     end
   end
 end
