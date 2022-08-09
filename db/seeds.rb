4212e0227424b9f4edb2ff0b0367cf70eae53acf
Alicia <arrowsrp@yahoo.com>
2022-08-09 15:27:50 -0700

    Fixes typo in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ puts "🌱 Seeding spices..."
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         email: Faker::Internet.email,
-        password: Faker::Internet.password(min_lengt: 8),
+        password: Faker::Internet.password(min_length: 8),
 
     )
 end
