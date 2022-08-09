d5678ad469cfdb3a2da1ba35be14d61f8ab6c8e5
Alicia <arrowsrp@yahoo.com>
2022-08-09 15:21:36 -0700

    Changes varchar datatype in users table to string

--- db/migrate/20220809215008_create_users.rb
+++ db/migrate/20220809215008_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :first_name 
       t.string :last_name
       t.string :email
-      t.varchar :password
+      t.string :password
     end
   end
 end
