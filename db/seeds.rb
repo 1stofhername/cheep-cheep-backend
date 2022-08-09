c03505a74656d71cd3c9349ab5a5ce76a275be8c
Alicia <arrowsrp@yahoo.com>
2022-08-09 15:11:23 -0700

    Adds seed data for followers table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
+require 'faker'
+
 puts "🌱 Seeding spices..."
 
+10.times do
+    User.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: Faker::Internet.email,
+        password: Faker::Internet.password(min_lengt: 8),
+
+    )
+end
+
+20.times do
+    Follower.create(
+        user_id: rand(1..10),
+        follower_id rand(1..10)
+    )
+end
 # Seed your database here
 
 puts "✅ Done seeding!"
