ffa021173e26c94179d4df847e4f0fb185260dd0
Alicia <arrowsrp@yahoo.com>
2022-08-09 22:28:41 -0700

    Adds get_messages method to message.rb;

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,4 +2,8 @@ class Conversation < ActiveRecord::Base
     has_many :messages
     has_many :users, through: :messages
     belongs_to :user
+
+    def get_messages
+        self.messages
+    end
 end
