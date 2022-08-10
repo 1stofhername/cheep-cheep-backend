d60401a0f41f455d13ae7d83b76d0941f3de6598
Alicia <arrowsrp@yahoo.com>
2022-08-09 21:38:22 -0700

    Adds message.rb model and cleans up user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
-
 class User < ActiveRecord::Base
     has_many :followers
     has_many :users, through: :followers
 
     def get_followers
        followers.each do |f|
-        user_id = f.follower_id
-        User.find_by(id: user_id.to_i)
+            user_id = f.follower_id
+            User.find_by(id: user_id.to_i)
+       end
     end
-    
-end
 
     
 end
