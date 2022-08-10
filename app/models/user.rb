193abd8a9c6ce418b16c3a9a9cba8e99bea1c3e2
Alicia <arrowsrp@yahoo.com>
2022-08-09 22:50:53 -0700

    Adds find_users_by_name and find_users_by_email function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
     has_many :messages
-    has_many :conversations
+    has_many :conversations, through: :messages
 
+    def get_conversations
+        self.conversations
+    end
+
+    def self.find_users_by_first_name name
+        User.where("lower(first_name)=?", name.downcase).first
+    end
+
+    def self.find_users_by_email email
+        User.where("lower(email)=?", email.downcase).first
+    end
     
 end
